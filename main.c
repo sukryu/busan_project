0a6bd5234e23da5147ba00afcdcb042e77fc002c
sukryu <hjinhyeog162@gmail.com>
2024-05-07 14:56:43 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// 열차 길이.
+#define LEN_MIN 15
+#define LEN_MAX 50
+
+// 마동석 체력
+#define STM_MIN 0
+#define STM_MAX 5
+
+// 확률
+#define PROB_MIN 10
+#define PROB_MAX 90
+
+// 어그로 범위
+#define AGGRO_MIN 0
+#define AGGRO_MAX 5
+
+// 마동석 이동 방향
+#define MOVE_LEFT 1
+#define MOVE_STAY 0
+
+// 좀비의 공격 대상
+#define ATK_NONE 0
+#define ATK_CITIZEN 1
+#define ATK_DONGSEOK 2
+
+// 마동석 행동
+#define ACTION_REST 0
+#define ACTION_PROVOKE 1
+#define ACTION_PULL 2
+
+int main() {
+  
+  
+  return 0;
+}
